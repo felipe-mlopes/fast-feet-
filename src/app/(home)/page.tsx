abdbbaf4a9f65d6ed730f3da69f0d52fdd723585
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-14 19:05:09 -0300

    feat: build home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,100 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Logo } from "@/components/global/logo";
+
+import { LogoIcon } from "@/components/icons/logo-icon";
+import { HamburguerIcon } from "@/components/icons/hamburguer-icon";
+import { Track } from "@/components/home/track";
+
 export default function Home() {
   return (
-    <>
-      <div>Home</div>
-    </>
+    <div className="space-y-8">
+      <header className="flex items-end justify-between w-full lg:row-start-1 lg:row-end-1 lg:col-start-1 lg:col-end-2 lg:w-auto lg:justify-start lg:gap-4">
+        <span className="flex items-end gap-2">
+          <LogoIcon />
+          <Logo />
+        </span>
+        <HamburguerIcon className="md:hidden" />
+      </header>
+      <main className="space-y-4">
+        <Track />
+        <section className="border-t-[1px] border-gray-light pt-4 md:border-0">
+          <Link href="#" className="relative">
+            <Image
+              src={"/img/deliveryman.jpg"}
+              alt=""
+              height={600}
+              width={600}
+              className="opacity-80"
+            />
+            <div className="absolute bottom-2 right-4 space-y-3">
+              <div className="space-y-2 text-center">
+                <h2 className="bg-orange-light text-gray-light p-1">
+                  Venha se tornar
+                </h2>
+                <h2 className="bg-orange-light text-gray-light p-1">
+                  nosso entregador
+                </h2>
+              </div>
+              <div className="bg-gray-light rounded-lg text-center font-bold">
+                Cadastre-se
+              </div>
+            </div>
+          </Link>
+        </section>
+        <section className="space-y-2 border-t-[1px] border-gray-light pt-4 md:border-0">
+          <h2 className="text-lg text-gray-light">
+            Por que ser um parceiro da Fast Feet?
+          </h2>
+          <strong className="text-gray-light">
+            Tenha a liberdade de entregar quando quiser e aumentar seus ganhos
+          </strong>
+          <div className="space-y-4 text-gray-light">
+            <div>
+              <i></i>
+              <div>
+                <strong>Complemente a sua renda</strong>
+                <p>Escolha suas entregas e receba os repasses semanalmente.</p>
+              </div>
+            </div>
+            <div>
+              <i></i>
+              <div>
+                <strong>Horários flexíveis</strong>
+                <p>
+                  Aceite apenas os pedidos que quiser, sem penalidades. Faça seu
+                  horário!
+                </p>
+              </div>
+            </div>
+            <div>
+              <i></i>
+              <div>
+                <strong>Ativação online</strong>
+                <p>
+                  Torne-se um motorista parceiro participando do processo de
+                  ativação de onde você estiver.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer className="flex flex-col gap-4 border-t-[1px] border-gray-light md:border-0 pt-5 text-center text-sm text-gray-light">
+        <div className="md:flex md:justify-center md:gap-2 w-full">
+          <p>© 2024 FastFeet</p>
+          <span className="md:before:content-['|']" />
+          <p>Todos os direitos reservados.</p>
+        </div>
+        <div className="md:flex md:justify-center md:gap-2 w-full">
+          <p>Av. Central, 1000 - Centro - Rio de Janeiro/RJ</p>
+          <span className="md:before:content-['|']" />
+          <p>CEP: 21000-000 | Tel. 4004-1020</p>
+          <span className="md:before:content-['|']" />
+          <p>CNPJ: 99.999.999/0001-00</p>
+        </div>
+      </footer>
+    </div>
   );
 }
