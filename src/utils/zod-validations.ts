1723def24105ca64bbc78dc9d1c094f4bcd4a33a
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-14 19:25:29 -0300

    refactor: change const name formSchemaLogin to formSchemaSignIn and create formSchemaSignUp zod validation const

--- src/utils/zod-validations.ts
+++ src/utils/zod-validations.ts
@@ -1,6 +1,18 @@
 import { z } from "zod";
 
-export const formSchemaLogin = z.object({
+export const formSchemaSignIn = z.object({
+    cpf: z
+        .string()
+        .refine((val) => val.length === 14, { message: "CPF inválido" })
+        .refine((val) => /^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(val), { message: "CPF inválido" }),
+    password: z
+        .string()
+        .min(6, { message: "A senha deve conter no mínimo 6 caracteres." })
+})
+
+export const formSchemaSignUp = z.object({
+    name: z.string(),
+    email: z.string().email({ message: "Deve conter o formato de um e-mail válido." }),
     cpf: z
         .string()
         .refine((val) => val.length === 14, { message: "CPF inválido" })
