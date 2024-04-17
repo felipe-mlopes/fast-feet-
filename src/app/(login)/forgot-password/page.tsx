ee5cba825dcefd8727569de30fb8421dd8b0ec9d
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-17 07:15:51 -0300

    refactor: update responsive styles css

--- src/app/(login)/forgot-password/page.tsx
+++ src/app/(login)/forgot-password/page.tsx
@@ -4,14 +4,14 @@ import { LongArrowIcon } from "@/components/icons/long-arrow-icon";
 
 export default function ForgotPassword() {
   return (
-    <div className="flex flex-col justify-between items-center gap-24 mx-8 my-20 lg:grid lg:grid-col-2 lg:grid-row-3 lg:justify-normal">
-      <main className="flex flex-col gap-16 lg:row-start-2 lg:row-end-3 lg:col-start-1 lg:col-end-1">
-        <div className="flex flex-col gap-6 lg:items-center">
+    <div className="flex flex-col justify-between items-center mt-6 gap-10 lg:mt-40">
+      <main className="flex flex-col gap-16 lg:flex-row lg:justify-center lg:items-center lg:w-full">
+        <div className="flex flex-col gap-6">
           <h2 className="flex flex-col text-5xl font-bold text-white italic">
             <span className="text-orange-light">Esqueceu</span>
             <span>sua senha?</span>
           </h2>
-          <p className="text-base font-normal text-lilac-smooth mr-[4.5rem]">
+          <p className="text-base font-normal text-lilac-smooth mr-[8.5rem] md:mr-0">
             Por motivos de segurança, para recurá-la, vá até a central da
             distribuidora Fastfeet.
           </p>
@@ -23,7 +23,7 @@ export default function ForgotPassword() {
           <p className="text-lilac-smooth">89 168-000</p>
         </div>
       </main>
-      <footer className="flex justify-between items-center w-full pt-24">
+      <footer className="flex justify-between items-center w-full pt-24 lg:justify-center lg:items-center lg:gap-16">
         <Link href={"/login"}>
           <LongArrowIcon />
         </Link>
