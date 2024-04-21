8efab4b06a66bbf840809b1c339f01ec940b0287
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-21 20:22:54 -0300

    refactor: rebuild html structure, include useForm of the react-hook-form, remove states of the useState and add useRef of the react

--- src/components/recipient/recipient-form.tsx
+++ src/components/recipient/recipient-form.tsx
@@ -1,144 +1,265 @@
 "use client";
 
-import {
-  DetailedHTMLProps,
-  FormHTMLAttributes,
-  PropsWithChildren,
-  useState,
-} from "react";
+import { useRef } from "react";
 import { useFormState } from "react-dom";
 
-import { FormStateTypes } from "@/types";
+import { Color } from "@/types";
 
-import LoginInput from "../login/login-input";
+import { registerRecipient } from "@/data/actions/recipients";
+
+import { Button } from "../global/button";
 
 import { ProfileIcon } from "../icons/profile-icon";
 import { MailIcon } from "../icons/mail-icon";
+import { AddressIcon } from "../icons/address-icon";
+import { prepareFormData } from "@/utils/prepare-form-data";
+import { useFormRegisterRecipient } from "@/hooks/use-form-register-recipient";
 
-type HTMLFormProps = DetailedHTMLProps<
-  FormHTMLAttributes<HTMLFormElement>,
-  HTMLFormElement
->;
-
-interface FormRecipientProps
-  extends PropsWithChildren<Omit<HTMLFormProps, "action">> {
-  action: (
-    prevState: FormStateTypes,
-    formData: FormData
-  ) => Promise<FormStateTypes>;
-}
-
-export function RecipientForm({ children, action }: FormRecipientProps) {
-  const [state, formAction] = useFormState(action, {
+export function RecipientForm() {
+  const [state, formAction] = useFormState(registerRecipient, {
     data: null,
     error: null,
   });
 
-  const [isOpen, setIsOpen] = useState(false);
-  const [isNameFilled, setIsNameFilled] = useState(false);
-  const [isEmailFilled, setIsEmailFilled] = useState(false);
-  const [isZipCodeFilled, setIsZipCodeFilled] = useState(false);
-  const [isAddressFilled, setIsAddressFilled] = useState(false);
-  const [isCityFilled, setIsCityFilled] = useState(false);
-  const [isStateFilled, setIsStateFilled] = useState(false);
-  const [isNeighborhoodFilled, setIsNeighborhoodFilled] = useState(false);
+  const { handleSubmit, values, register, errors, isSubmitting } =
+    useFormRegisterRecipient();
 
-  function handleClose() {
-    setIsOpen(!isOpen);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  function handleFormSubmit(formData: FormData) {
+    const data = prepareFormData(formData);
+
+    formAction(data);
   }
 
   return (
-    <form action={formAction} className="space-y-10">
+    <form
+      ref={formRef}
+      action={formAction}
+      onSubmit={handleSubmit((data) =>
+        handleFormSubmit(new FormData(formRef.current!))
+      )}
+      className="flex flex-col gap-10"
+    >
       <div className="space-y-4 text-gray-light">
         <div className="space-y-2 w-full">
-          <label>Nome Completo</label>
-          <LoginInput
-            inputType="text"
-            type="text"
-            name="name"
-            minLength={3}
-            onFilled={setIsNameFilled}
-          >
-            <ProfileIcon inputFilled={isNameFilled} />
-          </LoginInput>
-        </div>
-        <div className="space-y-2 w-full">
-          <label>E-mail</label>
-          <LoginInput
-            inputType="text"
-            type="email"
-            name="email"
-            minLength={4}
-            onFilled={setIsEmailFilled}
-          >
-            <MailIcon inputFilled={isEmailFilled} />
-          </LoginInput>
-        </div>
-        <div className="space-y-2 w-full">
-          <label>CEP</label>
-          <LoginInput
-            inputType="text"
-            type="number"
-            name="zipcode"
-            minLength={3}
-            onFilled={setIsZipCodeFilled}
-          >
-            <ProfileIcon inputFilled={isZipCodeFilled} />
-          </LoginInput>
-        </div>
-        <div className="space-y-2 w-full">
-          <label>Endereço Completo</label>
-          <LoginInput
-            inputType="text"
-            type="text"
-            name="address"
-            minLength={3}
-            onFilled={setIsAddressFilled}
-          >
-            <ProfileIcon inputFilled={isAddressFilled} />
-          </LoginInput>
-        </div>
-        <div className="space-y-2 w-full">
-          <label>Bairro</label>
-          <LoginInput
-            inputType="text"
-            type="text"
-            name="neighborhood"
-            minLength={2}
-            onFilled={setIsNeighborhoodFilled}
-          >
-            <ProfileIcon inputFilled={isNeighborhoodFilled} />
-          </LoginInput>
-        </div>
-        <div className="flex gap-6">
-          <div className="space-y-2 flex-1 w-full">
-            <label>Cidade</label>
-            <LoginInput
-              inputType="text"
-              type="text"
-              name="city"
-              minLength={3}
-              onFilled={setIsCityFilled}
-            >
-              <ProfileIcon inputFilled={isCityFilled} />
-            </LoginInput>
+          <label htmlFor="">Nome Completo</label>
+          <div className="flex justify-between gap-4 w-full p-4 rounded bg-gray-light">
+            <div className="flex gap-4 items-center w-full">
+              <ProfileIcon
+                color={
+                  !!values.clientName
+                    ? errors.clientName && values.clientName
+                      ? Color.Error
+                      : Color.Ok
+                    : Color.Default
+                }
+              />
+              <span className="border-[1px] bg-bluish-gray rounded w-[1px] h-6" />
+              <input
+                type="text"
+                className="grow min-w-5 outline-none text-base font-normal text-purple-dark bg-gray-light appearance-none"
+                {...register("clientName")}
+              />
+            </div>
           </div>
-          <div className="space-y-2 flex-none w-40">
-            <label>Estado</label>
-            <LoginInput
-              inputType="text"
-              type="text"
-              name="state"
-              minLength={2}
-              maxLength={2}
-              onFilled={setIsStateFilled}
-            >
-              <ProfileIcon inputFilled={isStateFilled} />
-            </LoginInput>
+          {errors.clientName && (
+            <span className="pt-1 text-xs font-bold text-red-400">
+              {errors.clientName?.message}
+            </span>
+          )}
+        </div>
+        <div className="space-y-2 w-full">
+          <label htmlFor="">E-mail</label>
+          <div className="flex justify-between gap-4 w-full p-4 rounded bg-gray-light">
+            <div className="flex gap-4 items-center w-full">
+              <MailIcon
+                color={
+                  !!values.clientEmail
+                    ? errors.clientEmail && values.clientEmail
+                      ? Color.Error
+                      : Color.Ok
+                    : Color.Default
+                }
+              />
+              <span className="border-[1px] bg-bluish-gray rounded w-[1px] h-6" />
+              <input
+                type="text"
+                className="grow min-w-5 outline-none text-base font-normal text-purple-dark bg-gray-light appearance-none"
+                {...register("clientEmail")}
+              />
+            </div>
+          </div>
+          {errors.clientEmail && (
+            <span className="pt-1 text-xs font-bold text-red-400">
+              {errors.clientEmail?.message}
+            </span>
+          )}
+        </div>
+        <div className="space-y-2 w-full">
+          <label htmlFor="">CEP</label>
+          <div className="flex justify-between gap-4 w-full p-4 rounded bg-gray-light">
+            <div className="flex gap-4 items-center w-full">
+              <AddressIcon
+                color={
+                  !!values.zipcode
+                    ? errors.zipcode && values.zipcode
+                      ? Color.Error
+                      : Color.Ok
+                    : Color.Default
+                }
+              />
+              <span className="border-[1px] bg-bluish-gray rounded w-[1px] h-6" />
+              <input
+                type="text"
+                className="grow min-w-5 outline-none text-base font-normal text-purple-dark bg-gray-light appearance-none"
+                {...register("zipcode")}
+              />
+            </div>
+          </div>
+          {errors.zipcode && (
+            <span className="pt-1 text-xs font-bold text-red-400">
+              {errors.zipcode?.message}
+            </span>
+          )}
+        </div>
+        <div className="space-y-2 w-full">
+          <label htmlFor="">Endereço</label>
+          <div className="flex justify-between gap-4 w-full p-4 rounded bg-gray-light">
+            <div className="flex gap-4 items-center w-full">
+              <AddressIcon
+                color={
+                  !!values.address
+                    ? errors.address?.street && values.address.street
+                      ? Color.Error
+                      : Color.Ok
+                    : Color.Default
+                }
+              />
+              <span className="border-[1px] bg-bluish-gray rounded w-[1px] h-6" />
+              <input
+                type="text"
+                className="grow min-w-5 outline-none text-base font-normal text-purple-dark bg-gray-light appearance-none"
+                {...register("address.street")}
+              />
+            </div>
+          </div>
+          {errors.address?.street && (
+            <span className="pt-1 text-xs font-bold text-red-400">
+              {errors.address?.street?.message}
+            </span>
+          )}
+        </div>
+        <div className="flex gap-4 md:gap-6">
+          <div className="space-y-2 grow-0 max-w-24">
+            <label htmlFor="">Número</label>
+            <div className="flex justify-between gap-4 w-full p-4 rounded bg-gray-light">
+              <div className="flex gap-4 items-center w-full">
+                <input
+                  type="text"
+                  className="grow min-w-5 outline-none text-base font-normal text-purple-dark bg-gray-light appearance-none"
+                  {...register("address.number")}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="space-y-2 grow min-w-40">
+            <label htmlFor="">Complemento</label>
+            <div className="flex justify-between gap-4 w-full p-4 rounded bg-gray-light">
+              <div className="flex gap-4 items-center w-full">
+                <input
+                  type="text"
+                  className="grow min-w-5 outline-none text-base font-normal text-purple-dark bg-gray-light appearance-none"
+                  {...register("address.complement")}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        {errors.address?.number && (
+          <span className="pt-1 text-xs font-bold text-red-400">
+            {errors.address?.number?.message}
+          </span>
+        )}
+        <div className="space-y-2 w-full">
+          <label htmlFor="">Bairro</label>
+          <div className="flex justify-between gap-4 w-full p-4 rounded bg-gray-light">
+            <div className="flex gap-4 items-center w-full">
+              <AddressIcon
+                color={
+                  !!values.neighborhood
+                    ? errors.neighborhood && values.neighborhood
+                      ? Color.Error
+                      : Color.Ok
+                    : Color.Default
+                }
+              />
+              <span className="border-[1px] bg-bluish-gray rounded w-[1px] h-6" />
+              <input
+                type="text"
+                className="grow min-w-5 outline-none text-base font-normal text-purple-dark bg-gray-light appearance-none"
+                {...register("neighborhood")}
+              />
+            </div>
+          </div>
+          {errors.neighborhood && (
+            <span className="pt-1 text-xs font-bold text-red-400">
+              {errors.neighborhood?.message}
+            </span>
+          )}
+        </div>
+        <div className="flex gap-4 md:gap-6">
+          <div className="space-y-2 grow min-w-40">
+            <label htmlFor="">Cidade</label>
+            <div className="flex justify-between gap-4 w-full p-4 rounded bg-gray-light">
+              <div className="flex gap-4 items-center w-full">
+                <AddressIcon
+                  color={
+                    !!values.city
+                      ? errors.city && values.city
+                        ? Color.Error
+                        : Color.Ok
+                      : Color.Default
+                  }
+                />
+                <span className="border-[1px] bg-bluish-gray rounded w-[1px] h-6" />
+                <input
+                  type="text"
+                  className="grow min-w-5 outline-none text-base font-normal text-purple-dark bg-gray-light appearance-none"
+                  {...register("city")}
+                />
+              </div>
+            </div>
+            {errors.city && (
+              <span className="pt-1 text-xs font-bold text-red-400">
+                {errors.city?.message}
+              </span>
+            )}
+          </div>
+          <div className="space-y-2 grow-0 max-w-24">
+            <label htmlFor="">UF</label>
+            <div className="flex justify-between gap-4 w-full p-4 rounded bg-gray-light">
+              <div className="flex gap-4 items-center w-full">
+                <input
+                  type="text"
+                  className="grow min-w-5 outline-none text-base font-normal text-purple-dark bg-gray-light appearance-none"
+                  {...register("state")}
+                />
+              </div>
+            </div>
+            {errors.state && (
+              <span className="pt-1 text-xs font-bold text-red-400">
+                {errors.state?.message}
+              </span>
+            )}
           </div>
         </div>
       </div>
-      {children}
+      <Button
+        content="Registar o destinatário"
+        type="submit"
+        disabled={isSubmitting}
+      />
     </form>
   );
 }
