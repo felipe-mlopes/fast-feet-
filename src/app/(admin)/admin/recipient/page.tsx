d173e87830d96bbd9ff7b7fbbbe5038a716b8f11
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-21 20:12:01 -0300

    refactor: remove Header and Button components and update main, Image and div css styles

--- src/app/(admin)/admin/recipient/page.tsx
+++ src/app/(admin)/admin/recipient/page.tsx
@@ -1,32 +1,23 @@
 import Image from "next/image";
 
-import { registerRecipient } from "@/data/actions/recipients";
-
-import { Button } from "@/components/global/button";
-import { Header } from "@/components/global/header";
 import { RecipientForm } from "@/components/recipient/recipient-form";
 
 export default function Recipient() {
   return (
-    <div className="p-6 min-h-screen space-y-16">
-      <Header />
-      <main className="block lg:flex lg:justify-around lg:items-start lg:gap-8">
-        <Image
-          src={"/img/deliveryman2.jpg"}
-          alt=""
-          width={1000}
-          height={1500}
-          className="hidden lg:block lg:w-[500px] lg:h-[720px] lg:rounded"
-        />
-        <div className="space-y-8 md:space-y-12 md:px-72 lg:px-0">
-          <h2 className="text-xl text-gray-light">
-            Faça o registro de um novo destinatário:
-          </h2>
-          <RecipientForm action={registerRecipient}>
-            <Button content="Registar o destinatário" type="submit" />
-          </RecipientForm>
-        </div>
-      </main>
-    </div>
+    <main className="flex justify-center pb-4 lg:justify-around lg:items-start lg:gap-8">
+      <Image
+        src={"/img/deliveryman2.jpg"}
+        alt=""
+        width={1000}
+        height={1500}
+        className="hidden lg:block lg:w-[500px] lg:h-full lg:rounded"
+      />
+      <div className="space-y-8 max-w-[80%] md:flex md:flex-col md:items-center md:gap-12  md:space-y-0 lg:block lg:max-w-full">
+        <h2 className="text-xl text-gray-light">
+          Faça o registro de um novo destinatário:
+        </h2>
+        <RecipientForm />
+      </div>
+    </main>
   );
 }
