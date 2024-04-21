5a429cc6ea4842e5347ef3e6fa98eb69aef923e5
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-21 20:08:49 -0300

    feat: create admin layout

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+import { Header } from "@/components/global/header";
+import { Cover } from "@/components/global/cover";
+
+export default function AdminLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-col items-center gap-12 pt-4 md:grid md:grid-col-2 md:grid-row-3 md:justify-normal md:max-h-[860px]">
+      <Header />
+      <Cover />
+      {children}
+    </div>
+  );
+}
