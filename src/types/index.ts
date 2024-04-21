a6429eb1f0f02a18f193b2e912759afcca89b34d
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-21 20:24:29 -0300

    feat: include color enum

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,9 @@ export interface FormStateTypes {
     data?: string | null
     error?: ValidationError[] | null
 }
+
+export enum Color {
+    Default = "#FFC042",
+    Ok = "#4C33CC",
+    Error = "#EF4444",
+  }
