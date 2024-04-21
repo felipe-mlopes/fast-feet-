f0a1b6b2cc098d7ddf67cb13fe2937d8ed3536cf
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-21 20:28:17 -0300

    refactor: add trim in all schemas, include refine to match password with confimPassword in formSchemaSignUp, update formSchemaRegisterRecipient and create formSchemaOutputRegisterRecipient

--- src/utils/zod-validations.ts
+++ src/utils/zod-validations.ts
@@ -3,31 +3,56 @@ import { z } from "zod";
 export const formSchemaSignIn = z.object({
     cpf: z
         .string()
+        .trim()
         .refine((val) => val.length === 14, { message: "CPF inválido" })
         .refine((val) => /^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(val), { message: "CPF inválido" }),
     password: z
         .string()
+        .trim()
         .min(6, { message: "A senha deve conter no mínimo 6 caracteres." })
 })
 
 export const formSchemaSignUp = z.object({
-    name: z.string(),
-    email: z.string().email({ message: "Deve conter o formato de um e-mail válido." }),
+    name: z.string().trim(),
+    email: z.string().trim().email({ message: "Deve conter o formato de um e-mail válido." }),
     cpf: z
         .string()
+        .trim()
         .refine((val) => val.length === 14, { message: "CPF inválido" })
         .refine((val) => /^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(val), { message: "CPF inválido" }),
     password: z
         .string()
-        .min(6, { message: "A senha deve conter no mínimo 6 caracteres." })
+        .trim()
+        .min(6, { message: "A senha deve conter no mínimo 6 caracteres." }),
+    confirmPassword: z
+        .string()
+        .trim()
+        .min(6, { message: "A senha deve conter no mínimo 6 caracteres." })    
+}).refine(({ password,  confirmPassword}) => password === confirmPassword, {
+    message: "As senhas precisam ser iguais.",
+    path: ["confirmPassword"]
 })
 
 export const formSchemaRegisterRecipient = z.object({
-    clientName: z.string(),
-    clientEmail: z.string().email(),
+    clientName: z.string().trim().min(4, 'Favor informar um nome válido.'),
+    clientEmail: z.string().trim().email({ message: 'Favor informar um e-mail válido.' }),
     zipcode: z.coerce.number().refine((cep) => String(cep).length === 8, "CEP deve ter 8 dígitos"),
+    address: z.object({
+        street: z.string().trim().min(3, 'Favor informar um endereço válido.'),
+        number: z.string().trim().min(1, 'Favor informar um número válido.'),
+        complement: z.string().trim()
+    }),
+    neighborhood: z.string().trim().min(3, 'Favor informar um bairro válido.'),
+    city: z.string().trim().min(2, 'Favor informar um cidade válida.'),
+    state: z.string().trim().toUpperCase().refine(st => st.length === 2, "A UF deve conter 2 dígitos válidos."),
+})
+
+export const formSchemaOutputRegisterRecipient = z.object({
+    clientName: formSchemaRegisterRecipient.shape.clientName,
+    clientEmail: formSchemaRegisterRecipient.shape.clientEmail,
+    zipcode: formSchemaRegisterRecipient.shape.zipcode,
     address: z.string(),
-    neighborhood: z.string(),
-    city: z.string(),
-    state: z.string(),
+    neighborhood: formSchemaRegisterRecipient.shape.neighborhood,
+    city: formSchemaRegisterRecipient.shape.city,
+    state: formSchemaRegisterRecipient.shape.state
 })
