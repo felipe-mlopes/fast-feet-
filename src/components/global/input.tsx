1161cf0226da66ad5aff174d6b73a4b96c8be6d3
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-22 20:19:58 -0300

    feat: create input component

---
+++ src/components/global/input.tsx
@@ -0,0 +1,41 @@
+import React, {
+  ForwardRefRenderFunction,
+  InputHTMLAttributes,
+  forwardRef,
+} from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  hasIcon?: boolean;
+  inputType?: string;
+  onShowPasswordToggle?: () => void;
+}
+
+const Input: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { hasIcon = true, inputType, onShowPasswordToggle, children, ...props },
+  ref
+) => {
+  const [prefix, suffix] = React.Children.toArray(children);
+
+  return (
+    <div className="flex justify-between gap-4 w-full p-4 rounded bg-gray-light">
+      <div className="flex gap-4 items-center w-full">
+        {prefix}
+        <span
+          className={`${
+            hasIcon
+              ? "border-[1px] bg-bluish-gray rounded w-[1px] h-6"
+              : "hidden"
+          }`}
+        />
+        <input
+          ref={ref}
+          className="grow min-w-5 outline-none text-base font-normal text-purple-dark bg-gray-light appearance-none"
+          {...props}
+        />
+      </div>
+      {suffix}
+    </div>
+  );
+};
+
+export default forwardRef(Input);
