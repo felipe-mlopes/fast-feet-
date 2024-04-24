4521bc6b839fdec174eb03532668021f47452d93
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-23 21:44:25 -0300

    feat: create order admin page

---
+++ src/app/(admin)/admin/order/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+import { OrderForm } from "@/components/order/order-form";
+
+export default function Order() {
+  return (
+    <main className="flex justify-center pb-4 lg:justify-around lg:items-start lg:gap-8">
+      <Image
+        src={"/img/deliveryman2.jpg"}
+        alt=""
+        width={1000}
+        height={1500}
+        className="hidden lg:block lg:w-[500px] lg:h-full lg:rounded"
+      />
+      <div className="space-y-8 max-w-[80%] md:flex md:flex-col md:items-center md:gap-12  md:space-y-0 lg:block lg:max-w-full">
+        <h2 className="text-xl text-gray-light">Crie um novo pedido:</h2>
+        <OrderForm />
+      </div>
+    </main>
+  );
+}
