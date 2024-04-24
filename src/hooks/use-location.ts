5820f3c4f5445fc3017de419a9335e349cba5f0d
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-23 21:51:20 -0300

    feat: create use location hook

---
+++ src/hooks/use-location.ts
@@ -0,0 +1,28 @@
+import { getCurrentPosition } from "@/utils/geolocation";
+import { getCityByCoordinates } from "@/utils/get-city-by-coordinates";
+import { useCallback, useEffect, useState } from "react";
+
+export function useLocation() {
+    const [location, setLocation] = useState("");
+
+    const handleGetLocation = useCallback(async () => {
+      const { lat, lng } = await getCurrentPosition({
+        enableHighAccuracy: true,
+      });
+  
+      const latitude = String(lat);
+      const longitude = String(lng);
+  
+      const coordinates = await getCityByCoordinates(latitude, longitude);
+  
+      setLocation(coordinates?.city);
+    }, []);
+  
+    useEffect(() => {
+      handleGetLocation();
+    });
+
+    return {
+        location
+    }
+}
