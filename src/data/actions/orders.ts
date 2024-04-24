addc3529816bdaf3b35a5518c4cfd345fcf8f219
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-23 21:50:17 -0300

    feat: add create order function and include use server

--- src/data/actions/orders.ts
+++ src/data/actions/orders.ts
@@ -1,5 +1,13 @@
+'use server'
+
+import { redirect } from "next/navigation"
+
 import { api } from "../api"
 import { getSession } from "./auth"
+
+import { formSchemaCreateOrder } from "@/utils/zod-validations"
+import { FormStateTypes } from "@/types"
+import { getRecipientByEmail } from "./recipients"
 
 export async function getOrdersPending(city: string) {
     const { token } = await getSession()
@@ -74,4 +82,45 @@ export async function getOrderByTrackingCode(trackingCode: string) {
     return {
         order: data.order
     }
+}
+
+export async function createOrder(prevState: FormStateTypes, formData: FormData): Promise<FormStateTypes> {
+    const { token } = await getSession()
+
+    const rawFormData = Object.fromEntries(formData.entries())
+    const result = formSchemaCreateOrder.safeParse(rawFormData)
+
+    if (!result.success) {
+        console.error(result.error.issues)
+        return { error: result.error.issues }
+    }
+
+    const { email, title } = result.data
+
+    const { recipient } = await getRecipientByEmail(email)
+
+    if (!recipient) {
+        redirect('/')
+    }
+
+    const response = await api('/orders', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            title,
+            recipientId: recipient.id
+        })
+    })
+
+    if (response.ok) {
+        redirect("/")
+
+    } else {
+        const data = await response.json()
+
+        return { error: data.error }
+        }
 }
