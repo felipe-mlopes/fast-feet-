59a58141f32a1f272b35b1e702060df46207c187
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-28 14:48:30 -0300

    refactor: create isSelectIconOpen and showOptions states, include setValue and emailsSearched of the useFormCreateOrder hook, create handleSelectClick function and change Input component to input html

--- src/components/order/order-form.tsx
+++ src/components/order/order-form.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useFormState } from "react-dom";
 
 import { createOrder } from "@/data/actions/orders";
-import { prepareFormData } from "@/utils/prepare-form-data";
 import { useFormCreateOrder } from "@/hooks/use-form-create-order";
+
 import { Color } from "@/types";
 
 import Input from "../global/input";
@@ -13,6 +13,7 @@ import { Button } from "../global/button";
 
 import { OrderIcon } from "../icons/order-icon";
 import { ProfileIcon } from "../icons/profile-icon";
+import { ArrowSelectIcon } from "../icons/arrow-select-icon";
 
 export function OrderForm() {
   const [state, formAction] = useFormState(createOrder, {
@@ -20,19 +21,30 @@ export function OrderForm() {
     error: null,
   });
 
+  const [isSelectIconOpen, setIsSelectIconOpen] = useState(false);
+  const [showOptions, setShowOptions] = useState(true);
+
   const {
     handleSubmit,
     register,
     errors,
     isSubmitting,
+    setValue,
     titleWatch,
     emailWatch,
+    emailsSearched,
   } = useFormCreateOrder();
 
   const formRef = useRef<HTMLFormElement>(null);
 
   function handleFormSubmit(formData: FormData) {
     formAction(formData);
+  }
+
+  function handleSelectClick(option: string) {
+    setValue("email", option);
+    setIsSelectIconOpen(!isSelectIconOpen);
+    setShowOptions(!showOptions);
   }
 
   return (
@@ -68,23 +80,43 @@ export function OrderForm() {
             </span>
           )}
         </div>
+
         <div className="space-y-2 w-full">
           <label htmlFor="email">Destinatário</label>
-          <Input
-            type="text"
-            placeholder="Pesquise pelo e-mail do destinatário"
-            {...register("email")}
-          >
-            <ProfileIcon
-              color={
-                !!emailWatch
-                  ? errors.email
-                    ? Color.Error
-                    : Color.Ok
-                  : Color.Default
-              }
-            />
-          </Input>
+          <div className="flex flex-col justify-between gap-4 relative w-full p-4 rounded bg-gray-light">
+            <div className="flex gap-4 items-center w-full">
+              <ProfileIcon
+                color={
+                  !!emailWatch
+                    ? errors.email
+                      ? Color.Error
+                      : Color.Ok
+                    : Color.Default
+                }
+              />
+              <span className="border-[1px] bg-bluish-gray rounded w-[1px] h-6" />
+              <input
+                className="grow min-w-5 outline-none text-base font-normal text-purple-dark bg-gray-light appearance-none"
+                {...register("email")}
+              />
+              <ArrowSelectIcon side={isSelectIconOpen ? "top" : "down"} />
+            </div>
+            {emailsSearched.length > 0 && showOptions && (
+              <ul className="absolute top-16 right-0 w-full p-1 rounded bg-gray-light text-purple-dark">
+                {emailsSearched.map((option, idx) => {
+                  return (
+                    <li
+                      key={idx}
+                      onClick={() => handleSelectClick(option.email)}
+                      className="p-1.5 hover:bg-orange-light hover:opacity-90 hover:rounded hover:cursor-pointer"
+                    >
+                      {option.email}
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+          </div>
           {errors.email && (
             <span className="pt-1 text-xs font-bold text-red-400">
               {errors.email?.message}
