e8990945e892ac84acf6081c7f7abcc1d0010ad9
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-28 14:26:09 -0300

    feat: create admin login page

---
+++ src/app/(admin)/admin/login/page.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+import { loginAction } from "@/data/actions/login";
+
+import { LoginForm } from "@/components/login/login-form";
+
+export default function Login() {
+  return (
+    <main className="flex flex-col gap-16 md:flex-row md:justify-between md:items-center md:pt-40 xl:justify-evenly">
+      <div className="flex flex-col gap-4">
+        {/*  <h2 className="flex flex-col text-5xl font-bold text-white italic">
+          <span className="text-orange-light">Entregador,</span>
+          <span>você é nosso maior valor</span>
+        </h2>
+        <p className="text-base font-normal text-lilac-smooth mr-28">
+          Faça seu login para começar suas entregas.
+        </p> */}
+      </div>
+      <LoginForm
+        action={loginAction}
+        className="flex flex-col gap-[1.625rem] pb-24 md:row-start-2 md:row-end-3 md:col-start-2 md:col-end-2 md:flex md:flex-col md:items-center md:mb-0 md:pb-0"
+      >
+        <div className="flex justify-between items-center">
+          <label htmlFor="remember" className="flex gap-3 items-center">
+            <input
+              type="checkbox"
+              id="remember"
+              name="remember"
+              className="w-5 h-5 rounded bg-gray-light border border-gray-light checked:border-[6px] checked:border-orange-light checked:gray-gray-light"
+            />
+            <span className="text-lilac-smooth text-xs">Lembrar-me</span>
+          </label>
+          <Link
+            href={"/forgot-password"}
+            className="text-lilac-smooth text-xs md:ml-8"
+          >
+            Esqueci minha senha
+          </Link>
+        </div>
+      </LoginForm>
+    </main>
+  );
+}
