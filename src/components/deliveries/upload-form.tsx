98ab2efe4179e5d3892d9018df0680ea58eb69f2
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-04-29 22:04:42 -0300

    feat: create upload form component

---
+++ src/components/deliveries/upload-form.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export async function UploadForm() {
+  return (
+    <form action="">
+      <input type="file" name="file" id="file" />
+      <input type="submit" value="upload" />
+    </form>
+  );
+}
