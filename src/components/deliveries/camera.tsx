d7fee51b3e45f8cbc1232a5c3576672c5eb19b21
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-05-02 07:38:31 -0300

    feat: create camera component

---
+++ src/components/deliveries/camera.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRef } from "react";
+
+import { UseCamera } from "@/hooks/use-camera";
+
+export function Camera() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const { mediaStream } = UseCamera();
+
+  if (
+    mediaStream &&
+    mediaStream instanceof MediaStream &&
+    videoRef.current &&
+    !videoRef.current.srcObject
+  ) {
+    videoRef.current.srcObject = mediaStream;
+  }
+
+  function handleCanPlay() {
+    videoRef.current?.play();
+  }
+
+  return (
+    <video
+      ref={videoRef}
+      onCanPlay={handleCanPlay}
+      muted
+      className="w-full h-full"
+    />
+  );
+}
