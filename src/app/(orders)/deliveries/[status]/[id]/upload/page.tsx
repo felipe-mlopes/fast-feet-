82c496317eb25c3ebf0396f09a204b988a2cc3da
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-05-02 07:38:20 -0300

    refactor: change path ref in Link, rebuild main html structure and include camera component

--- src/app/(orders)/deliveries/[status]/[id]/upload/page.tsx
+++ src/app/(orders)/deliveries/[status]/[id]/upload/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import { UploadForm } from "@/components/deliveries/upload-form";
 
 import { ArrowIcon } from "@/components/icons/arrow-icon";
-import { CameraIcon } from "@/components/icons/camera-icon";
 import { RepicIcon } from "@/components/icons/repic-icon";
+import { Camera } from "@/components/deliveries/camera";
 
 export default function Upload({ params }: { params: { orderId: string } }) {
   const isUpload = true;
@@ -12,23 +12,22 @@ export default function Upload({ params }: { params: { orderId: string } }) {
   return (
     <div className="h-screen overflow-hidden bg-gray-light">
       <header className="flex items-center justify-start gap-24 pl-5 pt-14 pb-12 relative bg-indigo-blue">
-        <Link href={`/deliveries`}>
+        <Link href={`/deliveries/pending`}>
           <ArrowIcon side="left" className="fill-white" />
         </Link>
         <h2 className="text-[1.625rem] text-white">Confirmar</h2>
       </header>
-      <main className="flex flex-col justify-around px-6 h-screen relative">
-        <div className="flex flex-col gap-4 w-[23rem] h-[68%] absolute -top-8 right-1/2 translate-x-1/2 rounded bg-gray-light shadow-card">
-          {/* <Image src="/img/screenshot.jpg" fill={true} alt="delivery picture" /> */}
-          <div className="flex justify-center items-center absolute bottom-0 left-1/2 -translate-y-1/2 -translate-x-1/2 z-10 w-16 h-16 rounded-full bg-black opacity-40">
-            {isUpload ? <CameraIcon /> : <RepicIcon />}
+      <main className="flex flex-col px-6 h-screen relative">
+        <div className="h-[68%]">
+          <div className="flex flex-col gap-4 w-[23rem] h-[68%] absolute -top-8 right-1/2 translate-x-1/2 rounded bg-gray-light shadow-card">
+            <Camera />
+            <div className="flex justify-center items-center absolute bottom-0 left-1/2 -translate-y-1/2 -translate-x-1/2 z-10 w-16 h-16 rounded-full bg-black opacity-40">
+              {isUpload ? <UploadForm /> : <RepicIcon />}
+            </div>
           </div>
-        </div>
-        <div className="space-y-4 z-10 pt-[28rem]">
-          <p className="px-24 z-10 text-center font-normal text-[0.625rem] text-lavender-gray">
+          <p className="px-24 text-center font-normal text-[0.625rem] text-lavender-gray">
             Tire uma foto do pacote com a assinatura do destinatário.
           </p>
-          <UploadForm />
         </div>
       </main>
     </div>
