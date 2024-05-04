7f0f6324925b3970c636752cde5b05bfe44b771e
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-05-04 12:11:04 -0300

    refactor: remove isUpload variable, change div tags to UploadForm component and add Button component

--- src/app/(orders)/deliveries/[status]/[id]/upload/page.tsx
+++ src/app/(orders)/deliveries/[status]/[id]/upload/page.tsx
@@ -3,12 +3,9 @@ import Link from "next/link";
 import { UploadForm } from "@/components/deliveries/upload-form";
 
 import { ArrowIcon } from "@/components/icons/arrow-icon";
-import { RepicIcon } from "@/components/icons/repic-icon";
-import { Camera } from "@/components/deliveries/camera";
+import { Button } from "@/components/global/button";
 
 export default function Upload({ params }: { params: { orderId: string } }) {
-  const isUpload = true;
-
   return (
     <div className="h-screen overflow-hidden bg-gray-light">
       <header className="flex items-center justify-start gap-24 pl-5 pt-14 pb-12 relative bg-indigo-blue">
@@ -18,16 +15,14 @@ export default function Upload({ params }: { params: { orderId: string } }) {
         <h2 className="text-[1.625rem] text-white">Confirmar</h2>
       </header>
       <main className="flex flex-col px-6 h-screen relative">
-        <div className="h-[68%]">
-          <div className="flex flex-col gap-4 w-[23rem] h-[68%] absolute -top-8 right-1/2 translate-x-1/2 rounded bg-gray-light shadow-card">
-            <Camera />
-            <div className="flex justify-center items-center absolute bottom-0 left-1/2 -translate-y-1/2 -translate-x-1/2 z-10 w-16 h-16 rounded-full bg-black opacity-40">
-              {isUpload ? <UploadForm /> : <RepicIcon />}
-            </div>
-          </div>
+        <div className="max-w-[23rem] h-[70%] space-y-6 absolute -top-8 right-1/2 translate-x-1/2 rounded bg-gray-light shadow-card">
+          <UploadForm />
           <p className="px-24 text-center font-normal text-[0.625rem] text-lavender-gray">
             Tire uma foto do pacote com a assinatura do destinatário.
           </p>
+        </div>
+        <div className="max-w-[23rem] w-[50%] absolute bottom-44 right-1/2 translate-x-1/2">
+          <Button content="Enviar foto" disabled />
         </div>
       </main>
     </div>
