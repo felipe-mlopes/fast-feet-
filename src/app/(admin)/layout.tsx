980e6433f13563407c4f0b706a5710c2f7d5ca54
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-05-04 12:06:21 -0300

    refactor: add div padding left and right styles

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -5,7 +5,7 @@ import { Cover } from "@/components/global/cover";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col items-center gap-12 pt-4 md:grid md:grid-col-2 md:grid-row-3 md:justify-normal md:max-h-[860px]">
+    <div className="flex flex-col items-center gap-12 pt-4 px-8 md:grid md:grid-col-2 md:grid-row-3 md:justify-normal md:max-h-[860px]">
       <Header />
       <Cover />
       {children}
