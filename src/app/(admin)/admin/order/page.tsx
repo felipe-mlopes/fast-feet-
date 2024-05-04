5eaa8325ea8c9cd5c6fdbbf7de12fe4c1cd60cab
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-05-04 12:09:58 -0300

    refactor: remove max width in div styles and include width 100% and padding top in main styles

--- src/app/(admin)/admin/order/page.tsx
+++ src/app/(admin)/admin/order/page.tsx
@@ -4,7 +4,7 @@ import { OrderForm } from "@/components/order/order-form";
 
 export default function Order() {
   return (
-    <main className="flex justify-center pb-4 lg:justify-around lg:items-start lg:gap-8">
+    <main className="flex justify-center pt-16 pb-4 w-full md:w-auto lg:justify-around lg:items-start lg:gap-8">
       <Image
         src={"/img/deliveryman2.jpg"}
         alt=""
@@ -12,7 +12,7 @@ export default function Order() {
         height={1500}
         className="hidden lg:block lg:w-[500px] lg:h-full lg:rounded"
       />
-      <div className="space-y-8 max-w-[80%] md:flex md:flex-col md:items-center md:gap-12  md:space-y-0 lg:block lg:max-w-full">
+      <div className="space-y-8 md:flex md:flex-col md:items-center md:gap-12  md:space-y-0 lg:block lg:max-w-full">
         <h2 className="text-xl text-gray-light">Crie um novo pedido:</h2>
         <OrderForm />
       </div>
