97d8f06e7b5564855a317cff68aade42d21b552c
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-05-11 22:28:27 -0300

    refactor: remove content props, add onChangeSearchContent props, change to ForwardRefRenderFunction, include ref in input html attribute and include forwardRef

--- src/components/global/search-input.tsx
+++ src/components/global/search-input.tsx
@@ -1,34 +1,37 @@
 "use client";
 
-import { InputHTMLAttributes } from "react";
-
-import { UseSearchInput } from "@/hooks/use-search-input";
+import {
+  ForwardRefRenderFunction,
+  InputHTMLAttributes,
+  forwardRef,
+} from "react";
 
 import { SearchIcon } from "../icons/search-icon";
 
 interface SearchInputProps extends InputHTMLAttributes<HTMLInputElement> {
   isHome?: boolean;
-  content: string;
+  onChangeSearchContent?: () => void;
 }
 
-export function SearchInput({ isHome, content }: SearchInputProps) {
-  const { register } = UseSearchInput();
-
+const SearchInput: ForwardRefRenderFunction<
+  HTMLInputElement,
+  SearchInputProps
+> = ({ isHome, onChangeSearchContent, ...props }, ref) => {
   return (
     <div
       className={`flex items-center gap-4 w-full bg-white border rounded shadow-card 
       ${isHome ? "justify-between py-2 px-3" : "justify-around px-6 py-5"}`}
     >
       <input
-        type="search"
-        id="search"
-        placeholder={content}
+        ref={ref}
         className="outline-none appearance-none bg-white w-full"
-        {...register("search")}
+        {...props}
       />
       <button>
         <SearchIcon />
       </button>
     </div>
   );
-}
+};
+
+export default forwardRef(SearchInput);
