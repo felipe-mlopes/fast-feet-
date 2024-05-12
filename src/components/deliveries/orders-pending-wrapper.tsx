d2b9209afd48d47e88b9fe12aa08fc205e02717b
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-05-11 22:26:26 -0300

    refactor: change server component to client component, add search props, add orders state, create handleFetchOrders useCallback, include useEffect, change ordersPending to ordersFiltered and CardsWrapper componento to Card component

--- src/components/deliveries/orders-pending-wrapper.tsx
+++ src/components/deliveries/orders-pending-wrapper.tsx
@@ -1,29 +1,71 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+
+import { OrdersProps } from "@/data/types/orders";
 import { getOrdersPending } from "@/data/actions/orders";
 
-import { CardsWrapper } from "./cards-wrapper";
+import { Card } from "./card";
 
 interface OrdersPendingWrapperProps {
   city: string;
+  search: string;
 }
 
-export async function OrdersPendingWrapper({
+export function OrdersPendingWrapper({
   city,
+  search,
 }: OrdersPendingWrapperProps) {
-  const { ordersPending } = await getOrdersPending(city);
+  const [orders, setOrders] = useState<OrdersProps[]>([]);
+
+  const deliverymanCity = city && city.toLowerCase();
+
+  const handleFetchOrders = useCallback(async () => {
+    const { ordersPending } = await getOrdersPending(deliverymanCity);
+
+    if (!ordersPending) return;
+
+    setOrders(ordersPending);
+  }, [deliverymanCity]);
+
+  useEffect(() => {
+    handleFetchOrders();
+  }, [handleFetchOrders]);
+
+  const ordersFiltered = orders.filter((order) => {
+    if (!!search) {
+      return order.recipientNeighborhood.includes(search);
+    } else {
+      return orders;
+    }
+  });
 
   return (
     <>
       <div className="flex items-center justify-center gap-5 text-ligth-slate-gray">
         <span className="content=[''] w-1/3 h-[1px] bg-bluish-gray" />
         <p className="text-nowrap">
-          {ordersPending.length > 1
-            ? `${ordersPending.length} entregas`
-            : `${ordersPending.length} entrega`}
+          {ordersFiltered.length > 1
+            ? `${ordersFiltered.length} entregas`
+            : `${ordersFiltered.length} entrega`}
         </p>
         <span className="content=[''] w-1/3 h-[1px] bg-bluish-gray" />
       </div>
       <div className="flex flex-col items-center justify-center gap-4 pt-4 md:flex-row md:flex-wrap md:gap-6">
-        <CardsWrapper orders={ordersPending} />
+        {ordersFiltered.map((order: OrdersProps) => {
+          return (
+            <Card
+              key={order.id}
+              id={order.id}
+              title={order.title}
+              createdAt={order.createdAt}
+              status={order.status}
+              recipientZipcode={order.recipientZipcode}
+              recipientCity={order.recipientCity}
+              recipientNeighborhood={order.recipientNeighborhood}
+            />
+          );
+        })}
       </div>
     </>
   );
