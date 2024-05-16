b55192ffa26fb00e8814216eac37e1070f0575c4
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-05-15 22:17:19 -0300

    refactor: remove styles css in button tag

--- src/components/global/button.tsx
+++ src/components/global/button.tsx
@@ -11,11 +11,7 @@ export function Button({ content, ...props }: ButtonProps) {
   const { pending } = useFormStatus();
 
   return (
-    <button
-      className="w-full md:px-[8.25rem] py-[1.125rem] rounded whitespace-nowrap text-center bg-orange-light text-purple-dark hover:bg-orange-300 font-medium disabled:opacity-50"
-      disabled={pending}
-      {...props}
-    >
+    <button disabled={pending} {...props}>
       {pending ? "Carregando..." : content}
     </button>
   );
