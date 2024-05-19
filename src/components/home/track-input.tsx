301e09b03a4fffa9c6da8b0369c8a53b09fe4656
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-05-19 20:20:18 -0300

    refactor: remove link of the next, useState and handleChange function, add useTrackingCode hook, useRouter of the next navigation and handleRedirectToOrder function, change div to form tag as parent and Link to button tag, include errors zod validation with span tag and update styles css component

--- src/components/home/track-input.tsx
+++ src/components/home/track-input.tsx
@@ -1,43 +1,61 @@
 "use client";
 
-import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { FormHTMLAttributes } from "react";
 
-export function TrackInput() {
-  const [inputValue, setInputValue] = useState("");
+import { useTrackingCode } from "@/hooks/use-form-tracking-code";
 
-  function handleChange(value: string) {
-    setInputValue(value.toLowerCase());
+interface TrackInputProps extends FormHTMLAttributes<HTMLFormElement> {}
+
+export function TrackInput({ ...props }: TrackInputProps) {
+  const { register, trackingCodeWatch, handleSubmit, errors } =
+    useTrackingCode();
+
+  const router = useRouter();
+
+  function handleRedirectToOrder() {
+    router.push(`/order/${trackingCodeWatch}`);
   }
 
   return (
-    <div className="flex items-end gap-6 px-4 py-3 absolute -bottom-2 z-50 bg-gray-light rounded shadow-card">
-      <div className="flex gap-3">
+    <form
+      onSubmit={handleSubmit(handleRedirectToOrder)}
+      className="flex flex-col md:flex-row items-end gap-6 px-4 py-3 absolute xl:relative -bottom-2 xl:bottom-0 z-50 bg-gray-light rounded shadow-card"
+      {...props}
+    >
+      <div className="flex flex-col md:flex-row gap-3">
         <div>
           <legend className="text-sm text-lavender-gray">
             Código de rastreio
           </legend>
           <input
             type="search"
-            value={inputValue}
-            onChange={(e) => handleChange(e.target.value)}
             className="flex items-center justify-between py-2 px-3 gap-4 w-full border rounded outline-none appearance-none bg-white text-lavender-gray"
+            {...register("trackingCode")}
           />
+          {errors.trackingCode && (
+            <span className="pt-1 text-xs font-bold text-red-400">
+              {errors.trackingCode?.message}
+            </span>
+          )}
         </div>
         <div>
           <legend className="text-sm text-lavender-gray">Cidade</legend>
           <input
             type="text"
             className="flex items-center justify-between py-2 px-3 gap-4 w-40 border rounded outline-none appearance-none bg-white text-lavender-gray"
+            {...register("city")}
           />
         </div>
       </div>
-      <Link
-        href={inputValue.length === 0 ? "#" : `/order/${inputValue}`}
-        className="p-4 rounded-lg bg-orange-light font-bold text-lavender-gray"
-      >
-        Consultar
-      </Link>
-    </div>
+      <div>
+        <button
+          type="submit"
+          className="p-4 rounded-lg bg-orange-light font-bold text-lavender-gray"
+        >
+          Consultar
+        </button>
+      </div>
+    </form>
   );
 }
