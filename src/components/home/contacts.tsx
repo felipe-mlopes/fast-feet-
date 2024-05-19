2b4946c0011bb53bd39cb4c5b2b6c81aadcef6f7
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-05-19 20:08:36 -0300

    refactor: add padding and border bottom styles css in section tag

--- src/components/home/contacts.tsx
+++ src/components/home/contacts.tsx
@@ -2,7 +2,10 @@ import { ContactForm } from "./contact-form";
 
 export function Contacts() {
   return (
-    <section id="contact" className="flex flex-col items-center gap-10">
+    <section
+      id="contact"
+      className="flex flex-col items-center gap-10 p-8 border-b-[1px] md:border-b-0 border-gray-light"
+    >
       <div>
         <h2 className="text-3xl text-center text-gray-light">Contato</h2>
         <p className="text-lg text-center text-gray-light">
