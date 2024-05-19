e6bcfe407b674c4438fe4c761579d6eab0596911
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-05-19 20:07:52 -0300

    feat: add styles css in section tag

--- src/components/home/banner.tsx
+++ src/components/home/banner.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export function Banner() {
   return (
-    <section id="banner">
+    <section
+      id="banner"
+      className="py-8 border-b-[1px] md:border-b-0 border-gray-light"
+    >
       <Image
         src={"/img/package.jpg"}
         alt=""
