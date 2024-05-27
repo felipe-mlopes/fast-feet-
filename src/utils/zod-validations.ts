971db6cdd50954049a12e336696ae4e78c23b70f
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-05-27 08:07:16 -0300

    refactor: include toLowerCase method in email and add contact in formSchemaContact object

--- src/utils/zod-validations.ts
+++ src/utils/zod-validations.ts
@@ -104,6 +104,13 @@ export const formSchemaTrackingCode = z.object({
 
 export const formSchemaContact = z.object({
     name: z.string().trim().min(4, 'Favor informar um nome válido.'),
-    email: z.string().trim().email({ message: 'Favor informar um e-mail válido.' }),
+    email: z
+        .string()
+        .trim()
+        .email({ message: 'Favor informar um e-mail válido.' })
+        .toLowerCase(),
+    contact: z.enum(['Entregador', 'Destinatário'], {
+        errorMap: () => ({ message: 'Selecione uma opção válida.' }),
+    }),
     description: z.string().min(10, { message: 'A descrição deve conter pelo menos 10 caracteres.' })
 })
