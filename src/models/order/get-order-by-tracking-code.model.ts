e056c125423da8b8054562578709894ad2ba9b6c
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-03 23:49:10 -0300

    feat: create get order by tracking code model

---
+++ src/models/order/get-order-by-tracking-code.model.ts
@@ -0,0 +1,33 @@
+import { Order } from "../types/order"
+import { api } from "../api"
+
+interface GetOrderByTrackingCodeResponse {
+    orderByTrackingCode: Order | null
+}
+
+interface IGetOrderByTrackingCodeModel {
+    handle(trackingCode: string): Promise<GetOrderByTrackingCodeResponse>
+}
+
+export class GetOrderByTrackingCodeModel implements IGetOrderByTrackingCodeModel {
+    async handle(trackingCode: string): Promise<GetOrderByTrackingCodeResponse> {
+        const response = await api(`/recipient-query?trackingCode=${trackingCode}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        
+        if (response.ok) {
+            const data = await response.json()
+
+            return {
+                orderByTrackingCode: data.orders
+            }
+        }
+
+        return {
+            orderByTrackingCode: null
+        }
+    }
+}
