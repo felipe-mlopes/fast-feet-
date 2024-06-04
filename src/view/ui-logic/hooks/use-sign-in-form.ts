450f6967bae92284a518154f9f320eb64179884c
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-03 23:39:09 -0300

    refactor: move useSignInForm file to view/ui-logic, update imports paths and include useFormState, useRef, useStates and handle functions

---
+++ src/view/ui-logic/hooks/use-sign-in-form.ts
@@ -0,0 +1,61 @@
+import { useEffect, useRef, useState } from "react";
+import { useFormState } from "react-dom";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { signInAction } from "@/models/actions/sign-in.action";
+
+import { FormSignInProps, formSchemaSignIn } from "@/presenter/validations/sign-in.validation";
+
+import { cpfMask } from "@/view/ui-logic/utils/cpf-mask";
+
+export function useSignInForm() {   
+    const [state, formAction] = useFormState(
+        signInAction, {
+            data: null,
+            error: null,
+    });
+    
+    const { register, formState: { isSubmitting, errors }, watch, setValue } = useForm<FormSignInProps>({
+        criteriaMode: 'all',
+        mode: 'onBlur',
+        resolver: zodResolver(formSchemaSignIn)
+    })
+    
+    const cpfWatch = watch('cpf')
+    const passwordWatch = watch('password')
+    
+    useEffect(() => {
+        if (cpfWatch !== undefined) {
+            setValue('cpf', cpfMask(cpfWatch))
+        }
+    }, [cpfWatch, setValue])
+
+    const formRef = useRef<HTMLFormElement>(null);
+        
+    const [showPassword, setShowPassword] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+
+    function handleModal() {
+        setShowModal(!showModal);
+      }
+    
+    function toggleShowPassword() {
+        setShowPassword(!showPassword);
+    }
+
+    return {
+        formRef,
+        register,
+        isSubmitting,
+        errors,
+        cpfWatch,
+        passwordWatch,
+        handleModal,
+        toggleShowPassword,
+        showModal,
+        showPassword,
+        state,
+        formAction,
+    }
+}
