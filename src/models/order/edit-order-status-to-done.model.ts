315a14b9730440b73cde4c7a3ab38562e2719ef7
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-03 23:48:31 -0300

    feat: create edit order status to done model

---
+++ src/models/order/edit-order-status-to-done.model.ts
@@ -0,0 +1,26 @@
+import { api } from "../api"
+import { getSession } from "../auth/auth"
+
+interface IEditOrderStatusToDoneModel {
+    handle(orderId: string): Promise<boolean>
+}
+
+export class EditOrderStatusToDoneModel implements IEditOrderStatusToDoneModel {
+    async handle(orderId: string): Promise<boolean> {
+        const { token } = await getSession()
+
+        const response = await api(`/orders/${orderId}/done`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        })
+        
+        if (response.ok) {
+            return Promise.resolve(true)
+        }
+
+        return Promise.resolve(false)
+    }
+}
