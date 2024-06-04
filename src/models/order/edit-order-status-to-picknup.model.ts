5cfe0079f3e4fa0e10ec67e976c48ee312974ab5
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-03 23:48:46 -0300

    feat: create edit order status to pick'n up model

---
+++ src/models/order/edit-order-status-to-picknup.model.ts
@@ -0,0 +1,26 @@
+import { getSession } from "../auth/auth"
+import { api } from "../api"
+
+interface IEditOrderStatusToPicknUpModel {
+    handle(orderId: string): Promise<boolean>
+}
+
+export class EditOrderStatusToPicknUpModel implements IEditOrderStatusToPicknUpModel {
+    async handle(orderId: string): Promise<boolean> {
+        const { token } = await getSession()
+
+        const response = await api(`/orders/${orderId}/picknup`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        })
+        
+        if (response.ok) {
+            return Promise.resolve(true)
+        }
+
+        return Promise.resolve(false)
+    }
+}
