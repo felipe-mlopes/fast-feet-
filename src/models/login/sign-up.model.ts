a364f8a2f7f124a1f12bd49f11f38071a89c6888
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-03 23:48:00 -0300

    feat: create sign up model

---
+++ src/models/login/sign-up.model.ts
@@ -0,0 +1,35 @@
+import { api } from "../api"
+
+interface SignUpData {
+    name: string
+    email: string
+    cpf: string
+    password: string
+}
+
+interface ISignUpModel {
+    handle({ name, email, cpf, password }: SignUpData): Promise<boolean>
+}
+
+export class SignUpModel implements ISignUpModel {
+    async handle({ name, email, cpf, password }: SignUpData): Promise<boolean> {
+        const response = await api('/deliveryman', { 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                },
+            body: JSON.stringify({
+                name,
+                email,
+                cpf,
+                password
+            })
+        })
+        
+        if (response.ok) {
+            return Promise.resolve(true)
+        }
+
+        return Promise.resolve(false)
+    }
+}
