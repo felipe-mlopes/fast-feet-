6a0d4e6e91ee0f328300f64b4568374e81b17902
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-03 23:47:54 -0300

    feat: create sign in model

---
+++ src/models/login/sign-in.model.ts
@@ -0,0 +1,52 @@
+import { redirect } from "next/navigation"
+
+import { api } from "../api"
+import { getAuthData, getSession, setAuthData } from "../auth/auth"
+
+interface SignInData {
+    cpf: string
+    password: string
+}
+
+interface ISignInModel {
+    handle({ cpf, password }: SignInData): Promise<boolean>
+}
+
+export class SignInModel implements ISignInModel {
+    async handle({ cpf, password }: SignInData): Promise<boolean> {        
+        const session = await getSession()
+
+        const response = await api('/deliveryman/sessions', { 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                },
+            body: JSON.stringify({
+                cpf,
+                password
+            })
+        })
+        
+        if (response.ok) {
+            const data = await response.json()
+            await setAuthData(data.access_token);
+
+            const payload = await getAuthData()
+
+            if (payload?.role === 'ADMIN') {
+                redirect("/home")
+            }
+
+            session.token = data.access_token
+            session.sub = payload?.sub
+            session.role = payload?.role
+            session.isLoggedIn = true
+
+            await session.save()
+
+            return Promise.resolve(true)
+        }
+
+        return Promise.resolve(false)
+    }
+}
