da73cc4c76acb68224c33d279777ef0c09f4682a
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-04 23:37:45 -0300

    refactor: rename file useFormLSignUp to useSignUpForm

--- src/view/ui-logic/hooks/use-sign-up-form.ts
+++ src/view/ui-logic/hooks/use-sign-up-form.ts
@@ -1,13 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
+import { useFormState } from "react-dom";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { FormSignUpProps, formSchemaSignUp } from "@/presenter/validations/sign-up.validation";
 
 import { cpfMask } from "@/view/ui-logic/utils/cpf-mask";
+import { signUpAction } from "@/models/actions/sign-up.action";
 
-export function useFormSignUp() {
-    const { handleSubmit, register, formState: { isSubmitting, errors }, watch, setValue } = useForm<FormSignUpProps>({
+export function useSignUpForm() {
+    const [state, handleSignUp] = useFormState(signUpAction, {
+        data: null,
+        error: null,
+    });
+
+    const { register, formState: { isSubmitting, errors }, watch, setValue } = useForm<FormSignUpProps>({
         criteriaMode: 'all',
         mode: 'onBlur',
         resolver: zodResolver(formSchemaSignUp)
@@ -24,9 +31,16 @@ export function useFormSignUp() {
             setValue('cpf', cpfMask(cpfWatch))
         }
     }, [cpfWatch, setValue])
+    
+    const formRef = useRef<HTMLFormElement>(null);
+
+    const [showModal, setShowModal] = useState(false);
+
+    function handleModal() {
+    setShowModal(!showModal);
+    }
 
     return {
-        handleSubmit,
         register,
         isSubmitting,
         errors,
@@ -34,6 +48,11 @@ export function useFormSignUp() {
         cpfWatch,
         emailWatch,
         passwordWatch,
-        confirmPasswordWatch
+        confirmPasswordWatch,
+        formRef,
+        showModal,
+        handleModal,
+        state,
+        handleSignUp
     }
 }
