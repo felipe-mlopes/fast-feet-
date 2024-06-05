d77b30ce20605d6677624e91e00f8319e2a078e3
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-04 23:16:37 -0300

    refactor: update SignInPresenter import and change state to underscore in signInAction params

--- src/models/actions/sign-in.action.ts
+++ src/models/actions/sign-in.action.ts
@@ -1,11 +1,11 @@
 'use server'
 
-import { SignInFormPresenter } from "@/presenter/login/sign-in.presenter"
+import { SignInPresenter } from "@/presenter/login/sign-in.presenter"
 
 import { FormStateTypes } from "@/view/ui-logic/types/form-state"
 
-export async function signInAction(state: FormStateTypes, formData: FormData): Promise<FormStateTypes> {
-    const signInPresenter = new SignInFormPresenter()
+export async function signInAction(_: FormStateTypes, formData: FormData): Promise<FormStateTypes> {
+    const signInPresenter = new SignInPresenter()
 
     return await signInPresenter.handleFormSubmit(formData)
 }
