0625aa7a9846a5de4a83fa71d4bf2537d7518067
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-09 23:21:31 -0300

    fix: add FormStateTypes in function params

--- src/view/ui-logic/actions/sign-up.action.ts
+++ src/view/ui-logic/actions/sign-up.action.ts
@@ -4,7 +4,7 @@ import { SignUpController } from "@/presenter/login/sign-up.controller";
 
 import { FormStateTypes } from "@/view/ui-logic/types/form-state";
 
-export async function signUpAction(formData: FormData): Promise<FormStateTypes> {
+export async function signUpAction(_: FormStateTypes, formData: FormData): Promise<FormStateTypes> {
     const signUpPresenter = new SignUpController()
 
     return await signUpPresenter.execute(formData)
