aad2d84a1f767505445c5ab167cce11a0dd02b9b
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-09 23:12:30 -0300

    refactor: change LoginPresenter to LoginController class name, switch handleFormSubmit to execute method name and remove unused return

--- src/presenter/login/login.controller.ts
+++ src/presenter/login/login.controller.ts
@@ -6,14 +6,14 @@ import { formSchemaLogin } from "@/presenter/validations/login.validation";
 
 import { FormStateTypes } from "@/view/ui-logic/types/form-state";
 
-export class LoginPresenter {
+export class LoginController {
     private loginModel: LoginModel;
 
     constructor() {
         this.loginModel = new LoginModel();
     }
 
-    async handleFormSubmit(formData: FormData): Promise<FormStateTypes> {
+    async execute(formData: FormData): Promise<FormStateTypes> {
         const rawFormData = Object.fromEntries(formData.entries());
         const result = formSchemaLogin.safeParse(rawFormData);
 
@@ -30,7 +30,6 @@ export class LoginPresenter {
 
         if (isAuthenticated) {
             redirect("/admin");
-            // return { data: "success", error: null };
         } else {
             return { data: null, error: [{ path: [], message: "Authentication failed" }] };
         }
