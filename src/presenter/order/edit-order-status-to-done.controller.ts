a7e707ec25301abbd708efa115a1852cafe2d765
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-09 23:14:41 -0300

    feat: create to edit order status to done controller

---
+++ src/presenter/order/edit-order-status-to-done.controller.ts
@@ -0,0 +1,37 @@
+import { EditOrderStatusToDoneModel } from "@/models/order/edit-order-status-to-done.model";
+
+import { setIdSchema } from "@/presenter/validations/set-id.validation";
+
+import { ValidationError } from "@/view/ui-logic/types/form-state";
+
+interface EditOrderStatusToDoneControllerResponse {
+    data?: string | null
+    error?: ValidationError[] | null
+}
+
+export class editOrderStatusToDoneController {
+    private editOrderStatusToDoneModel: EditOrderStatusToDoneModel
+
+    constructor() {
+        this.editOrderStatusToDoneModel = new EditOrderStatusToDoneModel()
+    }
+
+    async handle(id: string): Promise<EditOrderStatusToDoneControllerResponse> {
+        const result = setIdSchema.safeParse(id)
+
+        if (!result.success) {
+            console.error(result.error.issues)
+            return { error: result.error.issues }
+        }
+
+        const { orderId } = result.data
+
+        const order = await this.editOrderStatusToDoneModel.execute(orderId)
+
+        if (order) {
+            return { data: 'Order status edited successfully.', error: null }
+        } else {
+            return { data: null, error: [{ path: [], message: "Fetch orders failed." }] }
+        }
+    }
+}
