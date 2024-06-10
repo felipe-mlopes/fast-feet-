8b266cda2a993e8155ce193ce84c38c2d554f877
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-09 23:20:13 -0300

    fix: update loginAction import path

--- src/view/ui-logic/hooks/use-login-form.ts
+++ src/view/ui-logic/hooks/use-login-form.ts
@@ -1,10 +1,11 @@
+import { useRef, useState } from "react";
 import { useFormState } from "react-dom";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { FormLoginProps, formSchemaLogin } from "@/presenter/validations/login.validation";
-import { useRef, useState } from "react";
-import { loginAction } from "@/models/actions/login.action";
+
+import { loginAction } from "@/view/ui-logic/actions/login.action";
 
 export function useLoginForm() {
     const [state, handleLoginForm] = useFormState(loginAction, {
