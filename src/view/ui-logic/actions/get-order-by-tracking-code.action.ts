6ceb8acfd677e1398445e66d0a96afcba5d10f70
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-11 07:19:46 -0300

    feat: create get order by tracking code action

---
+++ src/view/ui-logic/actions/get-order-by-tracking-code.action.ts
@@ -0,0 +1,32 @@
+'use server'
+
+import { Order } from "@/models/types/order"
+
+import { GetOrderByTrackingCodeController } from "@/presenter/order/get-order-by-tracking-code.controller"
+
+import { FormStateTypes, ValidationError } from "@/view/ui-logic/types/form-state"
+
+interface GetOrderByTrackingCodeActionResponse {
+    data?: Order | null
+    error?: ValidationError[] | null
+}
+
+export async function getOrderByTrackingCodeAction(state: GetOrderByTrackingCodeActionResponse, formData: FormData): Promise<GetOrderByTrackingCodeActionResponse> {
+    const getOrderByTrackingCodeController = new GetOrderByTrackingCodeController()
+
+    const result = await getOrderByTrackingCodeController.handle(formData)
+
+    console.log(result)
+
+    if (result.error) {
+        return {
+            data: null,
+            error: [{ path: [], message: "Get order failed." }]
+        }
+    }
+
+    return {
+        data: result.data,
+        error: null,
+    }
+}
