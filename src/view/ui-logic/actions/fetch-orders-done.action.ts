88748f02e86dcddc0cd891cf59c35cdf8b088fa7
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-13 07:24:00 -0300

    feat: create fetch orders done action

---
+++ src/view/ui-logic/actions/fetch-orders-done.action.ts
@@ -0,0 +1,14 @@
+'use server'
+
+import { FetchOrdersDoneController } from "@/presenter/order/fetch-orders-done.controller"
+
+export async function fetchOrdersDoneAction(city: string) {
+    const fetchhOrdersDoneController = new FetchOrdersDoneController()
+
+    const { data, error } = await fetchhOrdersDoneController.handle(city)
+    
+    return {
+        data,
+        error
+    }
+}
