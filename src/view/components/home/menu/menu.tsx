ed095d9d8ee9bd89d242f5c24f1bfbcc9c433d5b
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-14 07:55:21 -0300

    refactor: remove useState from React and change menu modal logic to useMenuModal hook

--- src/view/components/home/menu/menu.tsx
+++ src/view/components/home/menu/menu.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+
+import { useMenuModal } from "@/view/ui-logic/hooks/use-menu-modal";
 
 import { MenuModal } from "@/view/components/home/menu/menu-modal";
 import { MenuMain } from "./menu-main";
@@ -9,15 +10,8 @@ import { MenuMain } from "./menu-main";
 import { HamburguerIcon } from "@/view/components/icons/hamburguer-icon";
 
 export function Menu() {
-  const [showMenuModal, setShowMenuModal] = useState(false);
-
-  function handleCloseMenuModal() {
-    setShowMenuModal(false);
-  }
-
-  function handleOpenMenuModal() {
-    setShowMenuModal(true);
-  }
+  const { showMenuModal, handleOpenMenuModal, handleCloseMenuModal } =
+    useMenuModal();
 
   return (
     <>
