b2ccbec56a40a014f970d368d0a571cde008cbf8
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-14 08:00:32 -0300

    refactor: add useRef from React, rename useFormContact to useContactForm function and include formRef in function returns

--- src/view/ui-logic/hooks/use-contact-form.ts
+++ src/view/ui-logic/hooks/use-contact-form.ts
@@ -1,17 +1,21 @@
 import { useForm } from "react-hook-form";
+import { useRef } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { ContactFormSchema, formSchemaContact } from "@/presenter/validations/send-contact.validation";
 
-export function useFormContact() {
+export function useContactForm() {
     const { register, handleSubmit, reset, formState: { errors } } = useForm<ContactFormSchema>({
         resolver: zodResolver(formSchemaContact)
     })
+
+    const formRef = useRef<HTMLFormElement>(null);
 
     return {
         register,
         handleSubmit,
         reset,
-        errors
+        errors,
+        formRef
     }
 }
