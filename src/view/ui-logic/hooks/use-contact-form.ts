2734f718a93ce9cb2862f3c82d7d4cece1be524e
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-15 19:13:02 -0300

    refactor: add SendEmailAdapter, useFormState from react-dom and useRef from react and include formRef, state and handleSendContactForm in hook returns

--- src/view/ui-logic/hooks/use-contact-form.ts
+++ src/view/ui-logic/hooks/use-contact-form.ts
@@ -1,21 +1,38 @@
-import { useForm } from "react-hook-form";
 import { useRef } from "react";
+import { useForm } from "react-hook-form";
+import { useFormState } from "react-dom";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { ContactFormSchema, formSchemaContact } from "@/presenter/validations/send-contact.validation";
 
+import { sendContactAction } from "@/view/ui-logic/actions/send-contact.action";
+import { SendEmailAdapter } from "@/presenter/adpaters/mailing/send-mail.adapter";
+
 export function useContactForm() {
+    const sendEmailAdapter = new SendEmailAdapter()
+
+    const [state, handleSendContactForm] = useFormState(sendContactAction, {
+	    data: null,
+	    error: null,
+    });
+
     const { register, handleSubmit, reset, formState: { errors } } = useForm<ContactFormSchema>({
         resolver: zodResolver(formSchemaContact)
     })
 
     const formRef = useRef<HTMLFormElement>(null);
 
+    if (state.data) {
+        sendEmailAdapter.sendEmail(state.data)
+    }
+
     return {
         register,
         handleSubmit,
         reset,
         errors,
-        formRef
+        formRef,
+        state,
+        handleSendContactForm
     }
 }
