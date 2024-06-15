8316e5e55e621ebdda719ece483d0157e7aa9ec5
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-15 19:05:10 -0300

    feat: create send contact action

---
+++ src/view/ui-logic/actions/send-contact.action.ts
@@ -0,0 +1,23 @@
+'use server'
+
+import { Email } from "@/presenter/adpaters/mailing/send-mail.adapter"
+import { SendContactFormController } from "@/presenter/controllers/contact/send-contact-form.controller"
+
+import { ValidationError } from "@/view/ui-logic/types/form-state"
+
+interface SendContactProps {
+    data?: Email | null
+    error?: ValidationError[] | null
+}
+
+export async function sendContactAction(_: SendContactProps, formData: FormData): Promise<SendContactProps> {
+    const sendContactFormController = new SendContactFormController()
+
+    const result = await sendContactFormController.handle(formData)
+
+    if (result) {
+        return { data: result.data, error: null }
+    } else {
+        return { data: null, error: [{ message: 'deu ruim' }] }
+    }
+}
