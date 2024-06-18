cd37ef3c76194b887b8cee3be5b17128a05ff567
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-17 22:40:19 -0300

    refactor: remove address object and include address_street, address_number and address_complement on register recipient form schema

--- src/presenter/validations/register-recipient.validation.ts
+++ src/presenter/validations/register-recipient.validation.ts
@@ -6,11 +6,9 @@ export const formSchemaRegisterRecipient = z.object({
     clientName: z.string().trim().min(4, 'Favor informar um nome válido.'),
     clientEmail: z.string().trim().email({ message: 'Favor informar um e-mail válido.' }),
     zipcode: z.coerce.number().refine((cep) => String(cep).length === 8, "CEP deve ter 8 dígitos"),
-    address: z.object({
-        street: z.string().trim().min(3, 'Favor informar um endereço válido.'),
-        number: z.string().trim().min(1, 'Favor informar um número válido.'),
-        complement: z.string().trim()
-    }),
+    address_street: z.string().trim().min(3, 'Favor informar um endereço válido.'),
+    address_number: z.string().trim().min(1, 'Favor informar um número válido.'),
+    address_complement: z.string().trim(),
     neighborhood: z.string().trim().min(3, 'Favor informar um bairro válido.'),
     city: z.string().trim().min(2, 'Favor informar um cidade válida.'),
     state: z.string().trim().toUpperCase().refine(st => st.length === 2, "A UF deve conter 2 dígitos válidos."),
