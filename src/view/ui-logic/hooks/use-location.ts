8b7e9d4e2e96a7d0101ef83e5671253eb862ce89
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-23 12:53:48 -0300

    refactor: remove usePathname and useRouter from next/navigation, remove useCallback and useState from react, change getCityByCoordinatesAction to setCityLocationAction and handleGetLocation to handleSetLocation

--- src/view/ui-logic/hooks/use-location.ts
+++ src/view/ui-logic/hooks/use-location.ts
@@ -1,51 +1,27 @@
-import { usePathname, useRouter } from "next/navigation";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import { getCurrentPosition } from "@/view/ui-logic/utils/geolocation";
-import { getCityByCoordinatesAction } from "@/view/ui-logic/actions/get-city-by-coordinates.action";
+import { setCityLocationAction } from "@/view/ui-logic/actions/set-city-location.action";
 
 export function useLocation() {
-    const [location, setLocation] = useState("");
-    const [error, setError] = useState("")
-
-    const handleGetLocation = useCallback(async () => {
+    const handleSetLocation = async () => {
       const result = await getCurrentPosition({
-        enableHighAccuracy: true,
-      });
+        enableHighAccuracy: true
+      })
 
       if (result.error) {
-        setError(result.error.message)
-        return
+        return console.error(result.error)
       }
 
       const { lat, lng } = result.data!
-  
-      const latitude = String(lat);
-      const longitude = String(lng);
-  
-      const coordinates = await getCityByCoordinatesAction(latitude, longitude);
-  
-      if (coordinates.data) {
-        setLocation(coordinates.data);
-      }
-    }, []);
 
-    const path = usePathname();
-    const router = useRouter();
-  
-    useEffect(() => {
-      handleGetLocation();
+      const latitude = String(lat)
+      const longitude = String(lng)
 
-      if (location) {
-        router.push(`${path}?city=${location}`);
-      }
-
-      if (error) {
-        alert('Favor permitir a sua localização no navegador!')
-      }
-    }, [handleGetLocation, location, error, router, path]);
-
-    return {
-        location,
+      await setCityLocationAction(latitude, longitude)
     }
+
+    useEffect(() => {
+      handleSetLocation()
+    }, [])
 }
