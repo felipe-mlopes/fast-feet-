aeca8b000516f1018f224c9c5bf2e9c01aa63c9c
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-23 12:51:55 -0300

    refactor: move to view/components/deliveries folder, change useLocation hook call without return, add city as DeliverymanInfo function props and change location to city in p tag html content

--- src/view/components/deliveries/deliveryman-info.tsx
+++ src/view/components/deliveries/deliveryman-info.tsx
@@ -4,15 +4,15 @@ import { useLocation } from "@/view/ui-logic/hooks/use-location";
 
 import { PinIcon } from "@/view/components/icons/pin-icon";
 
-export function DeliverymanInfo() {
-  const { location } = useLocation();
+export function DeliverymanInfo({ city }: { city: string }) {
+  useLocation();
 
   return (
     <div className="flex justify-between items-center">
       <h3 className="text-[2rem] text-white">Entregas</h3>
       <div className="flex items-center gap-2">
         <PinIcon />
-        <p className="text-lilac-smooth">{location}</p>
+        <p className="text-lilac-smooth">{city}</p>
       </div>
     </div>
   );
