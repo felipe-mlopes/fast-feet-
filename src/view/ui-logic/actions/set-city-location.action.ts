70968f7a42846024df6adf8f7292e8c432081678
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-24 07:04:43 -0300

    refactor: remove console.log and add httpOnly and secure in cookie options

--- src/view/ui-logic/actions/set-city-location.action.ts
+++ src/view/ui-logic/actions/set-city-location.action.ts
@@ -9,10 +9,11 @@ export async function setCityLocationAction(latitude: string, longitude: string)
 
     const coordinates = await getCityByCoordinatesAction(latitude, longitude);
 
-    console.log(coordinates)
-
     if (coordinates.data) {
-        cookieStore.set('city', coordinates.data)
+        cookieStore.set('city', coordinates.data, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production"
+        })
     }
 }
 
