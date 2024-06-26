2b2f639942dc1a49e91af87e44ff311b8e785d39
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-26 06:42:46 -0300

    refactor: remove cookies from next/headers, getIronSession from iron-session and SessionData and sessionOptions from auth models, add getSession from auth models, include new routes conditions and add more routers in matcher config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,56 @@
-import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-import { getIronSession } from "iron-session";
 
-import { SessionData, sessionOptions } from "./models/auth/auth";
-
+import { getSession } from "./models/auth/auth";
 
 export async function middleware(request: NextRequest) {
+    const { isLoggedIn, token } = await getSession();
 
-    const session = await getIronSession<SessionData>(
-        cookies(),
-        sessionOptions
-    )
-    
-    if (session.isLoggedIn === undefined) {
+    const arrayToken = token ? token?.split(".") : null
+    const tokenPayload = arrayToken ? JSON.parse(atob(arrayToken[1])) : null
+
+    const role = token ? tokenPayload.role : null
+
+    const signUp = request.nextUrl.pathname.startsWith('/signup') && isLoggedIn
+    const signIn = request.nextUrl.pathname.startsWith('/signin') && isLoggedIn
+    const login = request.nextUrl.pathname.startsWith('/login') && isLoggedIn
+    const deliveries = request.nextUrl.pathname.startsWith('/deliveries') && !isLoggedIn
+    const dashboard = request.nextUrl.pathname.startsWith('/admin') && !isLoggedIn
+    const recipient = request.nextUrl.pathname.startsWith('/admin/recipient') && !isLoggedIn
+    const order = request.nextUrl.pathname.startsWith('/admin/order') && !isLoggedIn
+    const userAdmin = request.nextUrl.pathname.startsWith('/deliveries') && role === 'ADMIN'
+    const userDeliveryman = request.nextUrl.pathname.startsWith('/admin') && role === 'DELIVERYMAN'
+
+    if (signUp || signIn || login) {
+        if (role === 'ADMIN') {
+            return NextResponse.redirect(new URL('/admin', request.url))
+        } else {
+            return NextResponse.redirect(new URL('/deliveries/pending', request.url))
+        }
+    }
+
+    if (deliveries) {
         return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    if (dashboard || recipient || order) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    if (userAdmin) {
+        return NextResponse.redirect(new URL('/admin', request.url))
+    }
+
+    if (userDeliveryman) {
+        return NextResponse.redirect(new URL('/deliveries/pending', request.url))
     }
 }
 
 export const config = {
     matcher: [
-        "/deliveries/:path*"
+        "/signin",
+        "/signup",
+        "/login",
+        "/admin/:path*",
+        "/deliveries/:path*",
     ]
 }
