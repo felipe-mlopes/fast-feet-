4b38a4fd474e225092e37eb1d30ac858c20a379e
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-06-26 23:35:52 -0300

    refactor: include Webcam component from react-webcam, add children from ReactNode as function props, include Button component, change to videoConstraints, handleCapturePhoto, webcamRef and url useUploadForm hook returns, change hasPhoto to url in conditions, change handleTakePhoto to handleCapturePhoto function onClick of the button tag html and change canvas tag html to picture and img tags html

--- src/view/components/deliveries/upload-form.tsx
+++ src/view/components/deliveries/upload-form.tsx
@@ -1,52 +1,75 @@
 "use client";
 
+import { ReactNode } from "react";
+import Webcam from "react-webcam";
+
 import { useUploadForm } from "@/view/ui-logic/hooks/use-upload-form";
+
+import { Button } from "../global/button";
 
 import { CameraIcon } from "@/view/components/icons/camera-icon";
 import { RepicIcon } from "@/view/components/icons/repic-icon";
 
-export function UploadForm() {
+export function UploadForm({ children }: { children: ReactNode }) {
   const {
-    photoRef,
-    videoRef,
-    hasPhoto,
-    handleCanPlay,
-    handleTakePhoto,
+    videoConstraints,
+    handleCapturePhoto,
+    webcamRef,
+    url,
     handleCleanPhoto,
   } = useUploadForm();
 
   return (
-    <form action="" className="relative h-full">
-      {!hasPhoto && (
-        <div className="h-full">
-          <video
-            ref={videoRef}
-            onCanPlay={handleCanPlay}
-            muted
-            className="w-full h-full rounded"
-          />
-          <button
-            onClick={handleTakePhoto}
-            className="flex justify-center items-center absolute bottom-0 left-1/2 -translate-y-1/2 -translate-x-1/2 z-10 w-16 h-16 rounded-full bg-black opacity-40 cursor-pointer"
-          >
-            <CameraIcon />
-          </button>
-        </div>
+    <form action="" className="space-y-6 relative h-full">
+      {!url && (
+        <>
+          <div className="h-full">
+            <Webcam
+              audio={false}
+              width={1024}
+              height={768}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+            />
+            <button
+              onClick={handleCapturePhoto}
+              className="flex justify-center items-center absolute bottom-0 left-1/2 -translate-y-1/2 -translate-x-1/2 z-10 w-16 h-16 rounded-full bg-black opacity-40 cursor-pointer"
+            >
+              <CameraIcon />
+            </button>
+          </div>
+          {children}
+        </>
       )}
-      {hasPhoto && (
-        <div className="h-full">
-          <canvas ref={photoRef} className="w-full h-full rounded" />
-          <button
-            onClick={handleCleanPhoto}
-            className="flex justify-center items-center absolute bottom-0 left-1/2 -translate-y-1/2 -translate-x-1/2 z-10 w-16 h-16 rounded-full bg-black opacity-40 cursor-pointer"
-          >
-            <RepicIcon />
-          </button>
-        </div>
+      {url && (
+        <>
+          <div>
+            <picture>
+              <img
+                src={url}
+                alt="foto capturada"
+                className="w-full h-full rounded"
+              />
+            </picture>
+            <button
+              onClick={handleCleanPhoto}
+              className="flex justify-center items-center absolute bottom-0 left-1/2 -translate-y-1/2 -translate-x-1/2 z-10 w-16 h-16 rounded-full bg-black opacity-40 cursor-pointer"
+            >
+              <RepicIcon />
+            </button>
+          </div>
+          {children}
+        </>
       )}
-
-      {/* <input type="file" name="file" id="myFile" className="hidden" /> */}
-      {/* <input type="submit" value="upload" /> */}
+      <div>
+        <Button
+          content="Enviar foto"
+          type="submit"
+          disabled={!!!url}
+          className="w-full md:px-[8.25rem] py-[1.125rem] rounded whitespace-nowrap text-center bg-orange-light text-purple-dark hover:bg-orange-300 font-medium disabled:opacity-50"
+        />
+      </div>
     </form>
   );
 }
